e2164b3086f1f7c55a1e0dae6f1c8e19a3fb9213
Dany Palma <danielpalmaromero@outlook.com>
2021-05-26 23:24:36 -0400

    Added Facial Recognition; Ignore Targeting.py-In Progress

--- targeting.py
+++ targeting.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+trained_data = cv2.CascadeClassifier('C:\\Users\\danie\\Desktop\\haarcascade_frontalface_default.xml')
+
+
 
 tCoord = (300,200)
 
@@ -21,11 +24,13 @@ def aPos(mask):
         yA = yA / count
     return (int(xA), int(yA))
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while(True):
 
     ret, frame = capture.read()
+    greyscale_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
  
     HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS_FULL)
 
@@ -33,7 +38,6 @@ while(True):
     upperV = np.array([255, 255, 255])
 
     maskVid = cv2.inRange(HSV, lowerV, upperV)
- 
 
     cv2.imshow('video HSV', HSV)
     cv2.imshow('video original', frame)
@@ -50,10 +54,22 @@ while(True):
     if coord != (0, 0):
         tCoord = coord
         roundBoi = cv2.circle(maskVid, coord, 25, (255, 255, 0), 2)
+        roundBoii = cv2.circle(frame, coord, 25, (255, 255 , 0), 2) 
     else:
-        roundBoi = cv2.circle(maskVid, tCoord, 25, (255, 0, 0), 2)
+        roundBoi = cv2.circle(maskVid, tCoord, 25, (255, 255, 0), 2)
+        roundBoii = cv2.circle(frame, tCoord, 25, (255, 255 , 0), 2) 
+    
 
     cv2.imshow('FINAL DISPLAY', maskVid)
+    cv2.imshow('final bruh', frame) 
+
+    #detect faces
+    face_coordinates = trained_data.detectMultiScale(greyscale_frame)
+    # x and y is the upper left corner, and w and h are the corresponding width
+    # and length of the rectangle
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0, 255, 0),2)
+        print("bruh")
 
     if cv2.waitKey(1) == 27:
         break
