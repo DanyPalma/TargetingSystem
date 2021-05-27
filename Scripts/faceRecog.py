715859157bef3e95a96ec86b30443dc9dbc507b0
Dany Palma <danielpalmaromero@outlook.com>
2021-05-27 11:25:37 -0400

    Update faceRecog.py

--- Scripts/faceRecog.py
+++ Scripts/faceRecog.py
@@ -1,28 +1,37 @@
  # github.com/DanyPalma #
 # github.com/cfrankovich #
 import cv2
+import tkinter
 from random import randrange
+
+# Helper Functions
+def exitCallBack():
+    cv2.destroyAllWindows()
+
+# boolean var to keep track of loop
+running = True
 #loading pre trained face data from opencv
 trained_data = cv2.CascadeClassifier('TargetingSystem/Sources/haarcascade_frontalface_default.xml')
-
 #grabbing webcam 
 webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-while True:
+while running:
     #reading webcam frames
     frame_read,frame = webcam.read()
 
-
-# convert to black and white
+    # convert to black and white
     greyscale_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-
-# detect faces
+    # detect faces
     face_cordinates = trained_data.detectMultiScale(greyscale_frame)
     #x and y is the upper left corner coordinate and w and h are the corresponding width and height of the rectangle
     for (x,y,w,h) in face_cordinates:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-# show image
+    # show image
     cv2.imshow("Final",frame)
     #agument 1 defines milliseconds
-    cv2.waitKey(1) # keeps imaage in the screen
+    
+    if cv2.waitKey(33) == ord('q'):
+        cv2.destroyAllWindows()
+        running = False
+
